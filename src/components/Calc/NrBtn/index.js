9d8f2a559a57fc0217b694b69b3a2bb99ac893eb
Dan <danpopa1998@gmail.com>
2017-10-09 23:49:10 +0300

    add roboto font

--- src/components/Calc/NrBtn/index.js
+++ src/components/Calc/NrBtn/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import FlatButton from 'material-ui/FlatButton';
+import styles from './styles.module.styl'
 
 class NrBtn extends Component {
     render () {
         return (
-            <FlatButton>
+            <FlatButton className={styles.btn}>
                 {this.props.number}
             </FlatButton>
         )
