e4281cdb5cca08a364a95b419d5673860a65029b
Dan <danpopa1998@gmail.com>
2017-10-09 21:58:18 +0300

    add mui

---
+++ src/components/Calc/Output/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import styles from './styles.module.styl'
+
+class Output extends Component {
+    render() {
+        return (
+            <div className={styles.output}>
+                Output
+            </div>
+        )
+    }
+}
+
+export default Output
--- src/components/Calc/index.js
+++ src/components/Calc/index.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react'
 import NrBtn from './NrBtn'
+import Output from './Output'
 import styles from './styles.module.styl'
 
 class Calc extends Component {
+  constructor () {
+    super()
+    this.state = {
+
+    }
+  }
   render () {
     return (
       <div className={styles.container}>
-        <NrBtn number={0} />
+        <Output />
+        <NrBtn number={7} />
+        <NrBtn number={8} />
+        <NrBtn number={9} />
+        <NrBtn number={'/'} />
+        <NrBtn number={'%'} />
+        <NrBtn number={4} />
+        <NrBtn number={5} />
+        <NrBtn number={6} />
+        <NrBtn number={'*'} />
+        <NrBtn number={'1/x'} />
         <NrBtn number={1} />
         <NrBtn number={2} />
+        <NrBtn number={3} />
+        <NrBtn number={'-'} />
+        <NrBtn number={'+'} />
+        <NrBtn number={','} />
+        <NrBtn number={0} />
+        <NrBtn number={'='} />
       </div>
     )
   }
